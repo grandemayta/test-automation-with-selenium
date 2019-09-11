6432f1b4b46b6a816cfd349989f0b654c0b4cf78
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-09-11 13:59:07 +0200

    rename page package

--- src/stepDefinitions/StepsDefs_Login.java
+++ src/stepDefinitions/StepsDefs_Login.java
@@ -1,9 +1,9 @@
 package stepDefinitions;
 
-import Pages.Page_Login;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import pages.Page_Login;
 
 public class StepsDefs_Login {
 	
